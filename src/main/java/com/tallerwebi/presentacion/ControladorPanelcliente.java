13db5b3c7c394bc27b386d1a2f8470eaa47be536
EricMaida <ericmaidasantos3@gmail.com>
2026-07-01 12:08:04 -0300

    se corrigen tamaños

--- src/main/java/com/tallerwebi/presentacion/ControladorPanelcliente.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorPanelcliente.java
@@ -1,22 +1,33 @@
 package com.tallerwebi.presentacion;
 
+import com.tallerwebi.dominio.Cordenandas;
 import com.tallerwebi.dominio.Direccion;
+import com.tallerwebi.dominio.ServicioBuscarSupermercado;
 import com.tallerwebi.dominio.ServicioUsuario;
+import com.tallerwebi.dominio.Supermercado;
+import java.util.List;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ControladorPanelcliente {
 
   private final ServicioUsuario servicioUsuario;
+  private final ServicioBuscarSupermercado servicioBuscarSupermercado;
 
   @Autowired
-  public ControladorPanelcliente(ServicioUsuario servicioUsuario) {
+  public ControladorPanelcliente(
+    ServicioUsuario servicioUsuario,
+    ServicioBuscarSupermercado servicioBuscarSupermercado
+  ) {
     this.servicioUsuario = servicioUsuario;
+    this.servicioBuscarSupermercado = servicioBuscarSupermercado;
   }
 
   @GetMapping("/panel-cliente")
@@ -62,6 +73,27 @@ public class ControladorPanelcliente {
   public ModelAndView irASupermercados() {
     ModelMap modelo = new ModelMap();
     modelo.put("direccion", new Direccion());
+    return new ModelAndView("panel__busqueda-supermercados", modelo);
+  }
+
+  @PostMapping("/panel-cliente/supermercados")
+  public ModelAndView procesarBusquedaSupermercados(
+    @ModelAttribute("direccion") Direccion direccion
+  ) {
+    ModelMap modelo = new ModelMap();
+    Cordenandas cordenandas = servicioBuscarSupermercado.obtenerCordenadaActual(direccion);
+    if (cordenandas != null) {
+      modelo.put("latitud", cordenandas.getLatitud());
+      modelo.put("longitud", cordenandas.getLongitud());
+
+      List<Supermercado> supermercados = servicioBuscarSupermercado.buscarSupermercadosCercanos(
+        cordenandas.getLatitud(),
+        cordenandas.getLongitud()
+      );
+      modelo.put("supermercados", supermercados);
+    }
+
+    modelo.put("direccion", direccion);
     return new ModelAndView("panel__busqueda-supermercados", modelo);
   }
 }
